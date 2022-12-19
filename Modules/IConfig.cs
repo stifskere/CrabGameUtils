a6cad40a02e05639e6c5efa316e04c34d71f45dc
Stif <stifskere@gmail.com>
2022-12-19 08:47:15 +0100

    First commit

---
+++ Modules/IConfig.cs
@@ -0,0 +1,46 @@
+﻿// ReSharper disable RedundantUsingDirective.Global
+
+global using System.IO;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Text;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using System.Threading.Tasks;
+global using BepInEx;
+global using BepInEx.Configuration;
+global using BepInEx.IL2CPP;
+global using Flurl.Util;
+global using HarmonyLib;
+global using Il2CppSystem;
+global using Il2CppSystem.Collections.Generic;
+global using UnityEngine;
+
+global using GameSettings = MonoBehaviourPublicObjomaOblogaTMObseprUnique;
+global using Input = UnityEngine.Input;
+global using SteamManager = MonoBehaviourPublicObInUIgaStCSBoStcuCSUnique;
+global using GameManager = MonoBehaviourPublicDi2UIObacspDi2UIObUnique;
+global using AssemblyCs = MonoBehaviourPublicGaprLi1ObGaprInUnique;
+global using ChatBox = MonoBehaviourPublicRaovTMinTemeColoonCoUnique;
+
+namespace CrabGameUtils.Modules;
+
+public interface IConfig
+{
+    protected static SteamManager Steam { get; set; } = SteamManager.Instance;
+    protected static Plugin Instance { get; set; } = null!;
+
+
+    /// <inheritdoc cref="ConfigFile"/>
+    protected static ConfigEntry<T> Bind<T>(string name, string key, T def, string desc)
+     => ConfigFile.CoreConfig.Bind(name, key, def, desc);
+}
+
+public class BepinexDetectionPatch {
+    [HarmonyPatch(typeof(MonoBehaviourPublicGataInefObInUnique), "Method_Private_Void_GameObject_Boolean_Vector3_Quaternion_0")]
+    [HarmonyPatch(typeof(MonoBehaviourPublicCSDi2UIInstObUIloDiUnique), "Method_Private_Void_0")]
+    [HarmonyPatch(typeof(MonoBehaviourPublicVesnUnique), "Method_Private_Void_0")]
+    [HarmonyPatch(typeof(MonoBehaviourPublicObjomaOblogaTMObseprUnique), "Method_Public_Void_PDM_2")]
+    [HarmonyPatch(typeof(MonoBehaviourPublicTeplUnique), "Method_Private_Void_PDM_32")]
+    [HarmonyPrefix] public static bool Prefix(System.Reflection.MethodBase __originalMethod) => false;
+}
---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using CrabGameUtils.Modules;
+
+namespace CrabGameUtils;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin, IConfig
+{
+    private static readonly ConfigEntry<string> Test = 
+        IConfig.Bind("text to say", "Main", "Hello World!", "The text you will see in the chat when you spawn");
+
+    public override void Load()
+    {
+        IConfig.Instance = this;
+        Harmony.CreateAndPatchAll(typeof(Plugin));
+        using Harmony harmony = new Harmony("PlayerInfo");
+        harmony.PatchAll();
+        harmony.PatchAll(typeof(BepinexDetectionPatch));
+    }
+
+    [HarmonyPatch(typeof(AssemblyCs), "Start"), HarmonyPostfix]
+    public static void Start()
+    {
+        ChatBox.Instance.ForceMessage(Test.Value);
+    }
+
+    [HarmonyPatch(typeof(AssemblyCs), "Update"), HarmonyPostfix]
+    public static void Update()
+    {
+        
+    }
+}
