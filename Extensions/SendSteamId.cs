f24614636e7f824e6ccfee1b9e366bbe43289957
Bostengard <david.parma05@gmail.com>
2022-12-22 22:50:58 +0100

    Spectators added to send steam id

--- Extensions/SendSteamId.cs
+++ Extensions/SendSteamId.cs
@@ -57,7 +57,9 @@ public class bSendSteamId : Extension
         string descriptionFields = string.Empty;
         foreach (KeyValuePair<ulong, MonoBehaviourPublicCSstReshTrheObplBojuUnique> player in GameManager.Instance.activePlayers)
             descriptionFields += $"\u001b[33mName\u001b[0m\u001b[30m:\u001b[0m \u001b[32m{player.Value.username ?? "username not found."}\u001b[0m\n\u001b[33mSteamID64\u001b[0m\u001b[30m:\u001b[0m \u001b[32m{player.Value.steamProfile.m_SteamID.ToString() ?? "user steam id not found"}\u001b[0m\n\u001b[33mNumber\u001b[0m\u001b[30m:\u001b[0m \u001b[32m #{player.Value.playerNumber.ToString()}\u001b[0m\n\n";
-        
+        foreach (KeyValuePair<ulong, MonoBehaviourPublicCSstReshTrheObplBojuUnique> player in GameManager.Instance.spectators)
+            descriptionFields += $"\u001b[33mName\u001b[0m\u001b[30m:\u001b[0m \u001b[32m{player.Value.username ??= "username not found."}\u001b[0m\n\u001b[33mSteamID64\u001b[0m\u001b[30m:\u001b[0m \u001b[32m{player.Value.steamProfile.m_SteamID.ToString() ?? "user steam id not found"}\u001b[0m\n\u001b[33mNumber\u001b[0m\u001b[30m:\u001b[0m \u001b[32m Spectator \u001b[0m\n\n";
+
         EmbedBuilder embed = new EmbedBuilder()
             .SetTitle("User list")
             .SetDescription($"**Here is a list of players for the game with code:** `{Steam.currentLobby.m_SteamID}`\n**This game has:** `{GameManager.Instance.activePlayers.Count} players`\n```ansi\n{descriptionFields}```")
