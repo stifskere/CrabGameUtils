9cd019b743f2dd5093ae2b1143ab32135a1095d9
Stif <stifskere@gmail.com>
2023-01-02 20:24:56 +0100

    Party mode (requires further optimization)

--- Extensions/Glow.cs
+++ Extensions/Glow.cs
@@ -1,33 +1,99 @@
-﻿using Color = UnityEngine.Color;
-using TimeSpan = System.TimeSpan;
+﻿
+using Color = UnityEngine.Color;
+using Object = UnityEngine.Object;
 
 namespace CrabGameUtils.Extensions;
 
 [ExtensionName("Player glow")]
 public class Glow : Extension
 {
-    private MethodRunDelay _addLightsDelay;
+    public ExtensionConfig<string> Key = new("key", "i", "the key to enable or disable ");
     
+    private bool _enabled = true;
+    private GameObject _sun = null!;
+    private Color _defaultAmbient;
+    private readonly System.Collections.Generic.Dictionary<int, Color> _defaultObjectColors = new();
+
     public override void Start()
     {
-        _addLightsDelay = new MethodRunDelay(TimeSpan.FromSeconds(3), DelayAdd);
+        if (!Enabled.Value) return;
+        
+        if (!System.Enum.TryParse(Key.Value.ToUpper(), out KeyCode _))
+        {
+            ThrowError("Party errored, the keycode is not valid.");
+            return;
+        }
+
+        _sun = GameObject.Find("Directional Light");
+        _defaultAmbient = RenderSettings.ambientLight;
+        
+        foreach (GameObject gameObject in Object.FindObjectsOfType<GameObject>())
+        {
+            MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+            if (!renderer) continue;
+            
+            _defaultObjectColors.Add(gameObject.GetInstanceID(), renderer.material.color);
+        }
+
+        ChatBox.Instance.ForceMessage($"<color=#00FFFF>Party Loaded, press {Key.Value} to go on drugs.</color>");
     }
 
     public override void Update()
     {
-        _addLightsDelay.WaitAndRun();
-    }
+        if (!Enabled.Value) return;
+        
+        if (Input.GetKeyDown(Key.Value.ToLower()) && !ChatBox.Instance.inputField.isFocused)
+        {
+            _enabled = !_enabled;
+            ChatBox.Instance.ForceMessage($"<color=orange>Party mode:</color> {(_enabled ? "<color=green>enabled</color>" : "<color=red>disabled</color>")}");
+        }
 
-    private void DelayAdd()
-    {
+        _sun.active = !_enabled;
+        RenderSettings.ambientLight = _enabled ? Color.black : _defaultAmbient;
+
         foreach (KeyValuePair<ulong, CPlayer> player in GameManager.Instance.activePlayers)
         {
-            Random currentRand = new Random();
-            Light lightComp = player.Value.gameObject.AddComponent<Light>();
-            lightComp.type = LightType.Point;
-            lightComp.color = new Color(currentRand.Next(255), currentRand.Next(255), currentRand.Next(255));
-            lightComp.intensity = 0.5f;
-            lightComp.range = 5;
+            GameObject light = GameObject.Find($"Light-{player.Key}");
+            
+            if (!_enabled)
+            {
+                if (light) Object.Destroy(light);
+                continue;
+            }
+            
+            if (!light) light = new GameObject($"Light-{player.Key}");
+            Light lightComp = light.GetComponent<Light>();
+
+            if (!lightComp)
+            {
+                lightComp = light.AddComponent<Light>();
+                lightComp.type = LightType.Point;
+                lightComp.intensity = 5;
+                lightComp.range = 30;
+                lightComp.bounceIntensity = 1;
+                lightComp.color = Color.HSVToRGB(UnityEngine.Random.Range(0f, 1f), 1, 1);
+            }
+            
+            Color.RGBToHSV(lightComp.color, out float value, out _, out _);
+            lightComp.color = value + Time.deltaTime / 5 > 1 ? Color.HSVToRGB(0, 1, 1) : Color.HSVToRGB(value + Time.deltaTime / 5, 1, 1);
+            light.transform.position = player.Value.transform.position + new Vector3(0, 2, 0);
+        }
+
+        foreach (GameObject gameObject in Object.FindObjectsOfType<GameObject>())
+        {
+            MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+            if (!renderer) continue;
+            
+            if (!_enabled)
+            {
+                int id = gameObject.GetInstanceID();
+                if (!_defaultObjectColors.ContainsKey(id)) continue;
+                renderer.material.color = _defaultObjectColors[id];
+                continue;
+            }
+            
+            Color.RGBToHSV(renderer.material.color, out float h, out _, out _);
+            renderer.material.color = Color.HSVToRGB(h + Time.deltaTime / 5 >= 1 ? 0 : h + Time.deltaTime / 5, 1, 1);
         }
     }
 }
