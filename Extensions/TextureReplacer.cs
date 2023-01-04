dd6ae23dd11cc1ddf701031e4be9934da094354c
TheCat <ion699503@gmail.com>
2023-01-04 20:11:03 +0100

    Commit Message

--- Extensions/TextureReplacer.cs
+++ Extensions/TextureReplacer.cs
@@ -12,6 +12,16 @@ public class TextureReplacer : Extension
     public TextureReplacer()
     {
         Events.ChatBoxSubmitEvent += GetChatMessageLocal;
+        
+        Textures = new();
+
+        foreach (Type texture in Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(TextureReplacerTexture))))
+        {
+            string name = texture.GetCustomAttributesData().First(a => a.AttributeType.Name == "TextureNameAttribute").ConstructorArguments.First().Value?.ToString() ?? texture.Name;
+            TextureReplacerTexture instance = (TextureReplacerTexture)System.Activator.CreateInstance(texture, null);
+            if (!Textures.ContainsKey(name.ToLower()))
+                Textures.Add(name.ToLower(), instance);
+        }
     }
     
     public override void Awake()
@@ -21,24 +31,13 @@ public class TextureReplacer : Extension
 
     public override void Start()
     {
-        Textures ??= new();
-
-        foreach (Type texture in Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(TextureReplacerTexture))))
+        if (Current != null) 
         {
-            string name = texture.GetCustomAttributesData().First(a => a.AttributeType.Name == "TextureNameAttribute").ConstructorArguments.First().Value?.ToString() ?? texture.Name;
-            TextureReplacerTexture instance = (TextureReplacerTexture)System.Activator.CreateInstance(texture, null);
-            if (!Textures.ContainsKey(name.ToLower()))
-                Textures.Add(name.ToLower(), instance);
-        }
-        
-        if (Current != null)
-        {
-            Current.Enabled = false;
-            Current.Disable();
+            Disable(Current);
             Current = null;
         }
 
-        foreach (TextureReplacerTexture texture in Textures.Values) texture.Start();
+        foreach (TextureReplacerTexture texture in Textures!.Values) texture.Start();
         ChatBox.Instance.ForceMessage("<color=#00FFFF>Texture replacer loaded, type \"!textures help\" for help.</color>");
     }
     
@@ -47,37 +46,37 @@ public class TextureReplacer : Extension
         Current?.Update();
     }
 
-    public static void GetChatMessageLocal(string text)
+    public static void ProcessCommand(string[] args)
     {
-        string[] args = text.Split(" ");
-
-        if (args[0].ToLower() != "!textures") return;
-
-        if (args.Length <= 1)
+        if (args.Length < 1)
         {
             ChatBox.Instance.ForceMessage(Textures!.Keys.Aggregate("<color=#00FFFF>--- Textures list ---</color>", (s, s1) => $"{s}\n<color=green>- {s1}</color>"));
             return;
         }
         
-        switch (args[1].ToLower())
+        switch (args[0].ToLower())
         {
             case "enable":
             {
-                string textureName = String.Join(" ", args[2..]).ToLower();
+                string textureName = String.Join(" ", args[1..]).ToLower();
+
+                if (textureName.Equals("random"))
+                {
+                    if (Textures!.Count == 0)
+                    {
+                        ChatBox.Instance.ForceMessage("<color=red>Can't</color>");
+                        return;
+                    }
+                    SetCurrent(Textures!.Values.ElementAt(UnityEngine.Random.Range(0, Textures.Count)));
+                    return;
+                }
+                
                 if (!Textures!.ContainsKey(textureName))
                 {
                     ChatBox.Instance.ForceMessage($"<color=red>Couldn't find any texture with \"{textureName}\"</color>");
                     return;
                 }
-
-                if (Current != null)
-                {
-                    Current.Enabled = false; 
-                    Current.Disable();
-                }
-                Current = Textures[textureName];
-                Current.Enabled = true;
-                Current.Enable();
+                SetCurrent(Textures[textureName]);
                 ChatBox.Instance.ForceMessage($"<color=green>{textureName} enabled.</color>");
             }
                 break;
@@ -88,17 +87,50 @@ public class TextureReplacer : Extension
                     ChatBox.Instance.ForceMessage("<color=yellow>Nothing to disable.</color>");
                     return;
                 }
-                Current.Enabled = false;
-                Current.Disable();
-                Current = null;
+                RemoveCurrent();
                 ChatBox.Instance.ForceMessage("<color=green>Current theme disabled.</color>");
             }
                 break;
-            
             case "help":
                 ChatBox.Instance.ForceMessage("<color=#00FFFF>--- Textures help ---</color>\n<color=green>!textures - Without arguments it will show you a list of available textures\n!textures enable name - Will enable \"name\" texture and disable current if there is.\n!textures disable - Will disable active texture if there is</color>.\n<color=#00FFFF>--- end ---</color>");
                 break;
+            default:
+                ChatBox.Instance.ForceMessage($"<color=yellow>Could not find function {args[0]}</color>");
+                break;
         }
+    }
+
+    public static void SetCurrent(TextureReplacerTexture texture)
+    {
+        if (Current != null) Disable(Current);
+        Current = texture;
+        Enable(Current);
+    }
+    
+    public static void RemoveCurrent()
+    {
+        Disable(Current!);
+        Current = null;
+    }
+
+    public static void Enable(TextureReplacerTexture texture)
+    {
+        texture.Enabled = true;
+        texture.Enable();
+    }
+
+    public static void Disable(TextureReplacerTexture texture)
+    {
+        texture.Enabled = false;
+        texture.Disable();
+    }
+
+    public static void GetChatMessageLocal(string text)
+    {
+        string[] args = text.Split(" ");
+
+        if (args[0].ToLower() != "!textures") return;
+        ProcessCommand(args[1..]);
     }
 }
 
