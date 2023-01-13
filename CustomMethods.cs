8d9ef05d3ab659d22ca2b8dfa9db3dce64412151
Memw <stifskere@gmail.com>
2023-01-13 15:23:17 +0100

    MoleMetodz

---
+++ CustomMethods.cs
@@ -0,0 +1,31 @@
+using Color = UnityEngine.Color;
+
+namespace CrabGameUtils;
+
+public static class CustomMethods
+{
+    public static uint RandomColor() => (uint)new Random().Next(0x0, 0xFFFFFF);
+    
+    public static float FixValue(float value, int min, int max)
+    {
+        if (value < min) return min;
+        if (value > max) return max;
+        return value;
+    }
+    
+    public static Vector3 FillRandom(this Vector3 vector, int min, int max)
+    {
+        vector.x = UnityEngine.Random.Range(min, max);
+        vector.y = UnityEngine.Random.Range(min, max);
+        vector.z = UnityEngine.Random.Range(min, max);
+        return vector;
+    }
+    
+    public static Color ToColor(this Vector3 vector)
+        => new(vector.x, vector.y, vector.z);
+
+    public static void RandomColor(ref this Color color)
+    {
+        color = Color.HSVToRGB(UnityEngine.Random.Range(0, 255), 255, 255);
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
-﻿using Exception = System.Exception;
+﻿using Color = UnityEngine.Color;
+using Exception = System.Exception;
 
 namespace CrabGameUtils;
 
@@ -89,6 +90,7 @@ public class BepinexDetectionPatch {
     [HarmonyPrefix] public static bool Prefix(MethodBase __originalMethod) => false;
 }
 
+// ReSharper disable UnusedMember.Global
 public static class CustomMethods
 {
     public static uint RandomColor() => (uint)new Random().Next(0x0, 0xFFFFFF);
@@ -99,7 +101,7 @@ public static class CustomMethods
         if (value > max) return max;
         return value;
     }
-
+    
     public static Vector3 FillRandom(this Vector3 vector, int min, int max)
     {
         vector.x = UnityEngine.Random.Range(min, max);
@@ -107,8 +109,13 @@ public static class CustomMethods
         vector.z = UnityEngine.Random.Range(min, max);
         return vector;
     }
-
-    public static UnityEngine.Color ToColor(this Vector3 vector)
+    
+    public static Color ToColor(this Vector3 vector)
         => new(vector.x, vector.y, vector.z);
+
+    public static void RandomColor(ref this Color color)
+    {
+        color = Color.HSVToRGB(UnityEngine.Random.Range(0, 255), 255, 255);
+    }
 }
 
